b19da64f97dc82e924ca90140e25c402c03cd995
SyedShahiq <shahiqurrehman@gmail.com>
2020-01-22 22:39:50 +0500

    Dropdown added in navbar

--- src/components/header.js
+++ src/components/header.js
@@ -7,14 +7,19 @@ class Header extends Component {
         <nav className="navbar navbar-expand-md navbar-dark bg-steel fixed-top">
           <div className="container">
             <a className="navbar-brand mr-4" href="/">Manitas</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
             <div className="collapse navbar-collapse" id="navbarToggle">
-              <div className="navbar-nav ml-auto">
-                <a className="nav-item nav-link">Home</a>
-                <a className="nav-item nav-link">Posts</a>
-              </div>
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown</a>
+                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="/">Actiosn</a>
+                    <a className="dropdown-item" href="/">Another action</a>
+                    <div className="dropdown-divider"></div>
+                    <a className="dropdown-item" href="/">Something else here</a>
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
         </nav>
