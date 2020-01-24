d451b4c37d3b7bc7461846034e1f4ffeadcc63b7
SyedShahiq <shahiqurrehman@gmail.com>
2020-01-24 16:34:10 +0500

    Logo added and slider improved

--- src/components/Slider/slider.js
+++ src/components/Slider/slider.js
@@ -12,6 +12,10 @@ class Slider extends Component {
                         <h1>{this.props.content.slider.first.heading_1}</h1>
                         <h3>{this.props.content.slider.first.heading_2}</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <div className="download">
+                          <a href="/" className="apple"><i className="fa fa-apple"></i>App Store</a>
+                          <a href="/" className="google-playstore"><i className="fa fa-android"></i>Google Play</a>
+                        </div>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -20,6 +24,10 @@ class Slider extends Component {
                         <h1>{this.props.content.slider.first.heading_1}</h1>
                         <h3>{this.props.content.slider.first.heading_2}</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <div className="download">
+                        <a href="/" className="apple"><i className="fa fa-apple"></i>App Store</a>
+                        <a href="/" className="google-playstore"><i className="fa fa-android"></i>Google Play</a>
+                        </div>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import './header.css'
+import './header.css';
 
 class Header extends Component {
   render() {
     return (
       <header className="site-header">
         <nav className="navbar navbar-expand-lg navbar-light container">
-          <a className="navbar-brand" href="/">Manitas</a>
+          <a className="navbar-brand" href="#">
+            <img className="col-sm-2" src={require('./logo.png')} alt=""/>
+          </a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
