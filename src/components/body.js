91a295ca5252cc91d6a4eec4ac3b1c9ba4b112ce
SyedShahiq <shahiqurrehman@gmail.com>
2020-01-26 01:13:12 +0500

    Services added in body

--- src/components/body.js
+++ src/components/body.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Slider from './Slider/slider';
+import Services from './Services/services';
 
 class Body extends Component {
   render() {
     return (
       <main role="main" className="main">
         <Slider content={this.props.content} />
+        <Services />
       </main>
     );
   }
--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ class Header extends Component {
       <header className="site-header">
         <nav className="navbar navbar-expand-lg navbar-light container">
           <a className="navbar-brand" href="#">
-            <img className="col-sm-2" src={require('./logo.png')} alt="" />
+            <img className="col-sm-2" src={require('./logo.png')} alt="Logo" />
           </a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -16,7 +16,7 @@ class Header extends Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link" href="#">Services</a>
+                <a className="nav-link" href="#services">Services</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">Why Us</a>
