7ac84d7df92d20310e3e2e750c964eda0d364a6a
SyedShahiq <shahiqurrehman@gmail.com>
2020-01-29 00:17:20 +0500

    Whyus section added with animations

---
+++ src/components/Whyus/whyus.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './whyus.css';
+class WhyUs extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div class="wrapper">
+                    <div class="column first-col">
+                        <h1>Title Here</h1>
+                        <p>content here</p>
+                    </div>
+                    <div class="column second-col">
+                        <h1>Title Here</h1>
+                        <p>content here</p>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+
+    }
+}
+export default WhyUs;
--- src/components/body.js
+++ src/components/body.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Slider from './Slider/slider';
 import Services from './Services/services';
+import WhyUs from './Whyus/whyus';
 
 class Body extends Component {
   render() {
@@ -8,6 +9,7 @@ class Body extends Component {
       <main role="main" className="main">
         <Slider content={this.props.content} />
         <Services language={this.props.language} />
+        <WhyUs />
       </main>
     );
   }
