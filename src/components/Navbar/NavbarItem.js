4e00c5f379d398e9914353e99e6d82e435bfbb49
SyedShahiq <shahiqurrehman@gmail.com>
2020-01-30 16:08:07 +0500

    Navbar Item component added

---
+++ src/components/Navbar/NavbarItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NavbarItem extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <li className="nav-item">
+                    <a className="nav-link" href={"#" + this.props.ref_link}>{this.props.content}</a>
+                </li>
+            </React.Fragment>
+        )
+    }
+}
+export default NavbarItem;
