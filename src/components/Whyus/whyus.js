cc4e126b98a349e34406738ece0f359bf5a894e0
SyedShahiq <shahiqurrehman@gmail.com>
2020-01-30 15:11:26 +0500

    Changes made to why us section

--- src/components/Whyus/whyus.js
+++ src/components/Whyus/whyus.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './whyus.css';
 class WhyUs extends Component {
     render() {
         return (
             <React.Fragment>
-                <div class="wrapper">
+                <div id="why-us" class="why-us wrapper">
+                    <h1>{this.props.content.why_us.heading}</h1>
                     <div class="column first-col">
                         <h1>Title Here</h1>
                         <p>content here</p>
