5789d6b254efd0aabf9c00ddd90ff08ac21d0f76
SyedShahiq <shahiqurrehman@gmail.com>
2020-02-03 17:51:55 +0500

    Dynamic content added for why us section

--- src/components/Whyus/whyus.js
+++ src/components/Whyus/whyus.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import './whyus.css';
 class WhyUs extends Component {
     render() {
+        var user_a_points = this.props.content.why_us.user_a.points.map((point) => {
+            return <li>{point}</li>
+        });
+        var user_b_points = this.props.content.why_us.user_b.points.map((point) => {
+            return <li>{point}</li>
+        })
         return (
             <React.Fragment>
                 <div id="why-us" className="why-us wrapper">
-                    <h1>{this.props.content.why_us.heading}</h1>
+                    <h1 className="heading">{this.props.content.why_us.heading}</h1>
                     <div className="column first-col">
-                        <h1>Title Here</h1>
-                        <p>content here</p>
+                        <ul>
+                            <h1>{this.props.content.why_us.user_a.heading}</h1>
+                            {user_a_points}
+                        </ul>
                     </div>
                     <div className="column second-col">
-                        <h1>Title Here</h1>
-                        <p>content here</p>
+                        <ul>
+                            <h1>{this.props.content.why_us.user_b.heading}</h1>
+                            {user_b_points}
+                        </ul>
                     </div>
                 </div>
             </React.Fragment>
